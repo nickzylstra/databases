5d8b51ecba2043f4c800971ac2b0f840045b3f0a
nickzylstra <nick.zylstra@gmail.com>
2019-11-14 15:05:23 -0800

    Passes first test

--- server/models/index.js
+++ server/models/index.js
@@ -8,13 +8,11 @@ const headers = {
   'access-control-max-age': 10
 };
 
-const getUserId = (username, response) => {
+const getUserId = (username) => {
   return new Promise((resolve, reject) => {
     var getId = `SELECT user_id FROM users WHERE name = "${username}"`;
     const callback = (err, results, fields) => {
       if (err) {
-        response.writeHead(400, headers);
-        response.end(err);
         reject(err);
       }
 
@@ -32,48 +30,54 @@ module.exports = {
       dbConnection.query(queryString, callback);
     }, // a function which produces all the messages
     post: function ({ username, message, roomname }, response) {
-      var getId = `SELECT user_id FROM users WHERE name = "${username}"`;
-      const callback = (err, results, fields) => {
-        debugger;
-        if (err) {
+      getUserId(username)
+        .then((userId) => {
+          const callback = (err, results, fields) => {
+            if (err) {
+              response.writeHead(400, headers);
+              response.end(err);
+            }
+
+            response.writeHead(201, headers);
+            response.end();
+
+          };
+          let createdAt = '2008-01-01 00:00:01';
+          let updatedAt = '2008-01-01 00:00:01';
+          let queryString = `INSERT INTO messages(text, created_at, updated_at, room, user_id) VALUES ("${message}", "${createdAt}", "${updatedAt}", "${roomname}", ${userId})`;
+
+          dbConnection.query(queryString, callback);
+        })
+        .catch( (err) => {
           response.writeHead(400, headers);
           response.end(err);
-        }
+        });
+    }
 
-        var queryString = `INSERT INTO messages(text, room, user_id) VALUES (${text}, ${roomname}, ${results[0].user_id})`;
-        var callback = () => {
-          response.writeHead(201, headers);
-          response.end();
-        };
-
-        dbConnection.query(queryString, callback);
-      };
-
-      dbConnection.query(getId, callback);
-    } // a function which can be used to insert a message into the database
+    // dbConnection.query(getId, callback);
+    // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
     get: function () { },
     post: function (username, response) {
-      getUserId(username, response).then((userId) => {
+      getUserId(username).then((userId) => {
         response.writeHead(200, headers);
         response.end('User already exists');
-      }
-      ).catch((err) => {
-        var queryString = `INSERT INTO users(name) VALUES ("${username}")`;
-        var callback = (err, results, fields) => {
-          debugger;
-          if (err) {
-            response.writeHead(400, headers);
-            response.end(err);
-          }
-          response.writeHead(201, headers);
-          response.end('Succsessfully created new user');
-        };
-        dbConnection.query(queryString, callback);
-      });
+      })
+        .catch((err) => {
+          var queryString = `INSERT INTO users(name) VALUES ("${username}")`;
+          var callback = (err, results, fields) => {
+            if (err) {
+              response.writeHead(400, headers);
+              response.end(err);
+            }
+            response.writeHead(201, headers);
+            response.end('Succsessfully created new user');
+          };
+          dbConnection.query(queryString, callback);
+        });
     }
   }
 };
