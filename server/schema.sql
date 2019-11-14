81345917fa68c7aa0f787585af9a2bc1edcb9100
nickzylstra <nick.zylstra@gmail.com>
2019-11-13 16:53:42 -0800

    Creates table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,41 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  user_id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  CONSTRAINT user_id PRIMARY KEY (user_id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  room_id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  CONSTRAINT room_id PRIMARY KEY (room_id)
+);
+
+CREATE TABLE messages (
+  message_id INTEGER NOT NULL AUTO_INCREMENT,
+  text VARCHAR(140) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  room_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (room_id) REFERENCES rooms(room_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  CONSTRAINT message_id PRIMARY KEY (message_id)
+);
 
 
-
+CREATE TABLE friendships (
+  friendship_id INTEGER NOT NULL AUTO_INCREMENT,
+  user1_id INTEGER NOT NULL,
+  user2_id INTEGER NOT NULL,
+  FOREIGN KEY (user1_id) REFERENCES users (user_id),
+  FOREIGN KEY (user2_id) REFERENCES users (user_id),
+  CONSTRAINT friendship_id PRIMARY KEY (friendship_id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
